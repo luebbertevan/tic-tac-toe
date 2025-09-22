d8b584dacfd8570ff5682cf34f52bc70691a0021
luebbertevan <luebbertevan@gmail.com>
2025-09-22 15:58:34 -0400

    tailwind grid and box styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-const Box = () => <div className='box-container'>X</div>
+const Box = () => (
+	<div className="h-24 w-24 bg-blue-400 flex items-center justify-center text-white font-bold rounded">
+		X
+	</div>
+);
 
 const Grid = () => {
-  return ( 
-    <div className='grid-container'>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-    </div>
-  )
-}
+	return (
+		<div className="grid grid-cols-3 gap-2 max-w-xl mx-auto p-4">
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+			<Box></Box>
+		</div>
+	);
+};
 
 function App() {
-  const [box, setBox] = useState(Box)
+	const [box, setBox] = useState(Box);
 
-  return ( 
-    <div>
-       <Grid/>
-       <h1 className="text-red-500">Hello Tailwind!</h1>
-  
-    </div>
-  )
+	return (
+		<div>
+			<Grid />
+			<h1 className="text-red-500">Hello Tailwind!</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
