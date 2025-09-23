a3a427f22c5119fef307b9af03eb4b4058e4594c
luebbertevan <luebbertevan@gmail.com>
2025-09-23 16:49:22 -0400

    make move is working

--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,23 +1,23 @@
 export type Player = "X" | "O";
 export type Cell = Player | null;
 
-export type TicTacToe {
+export type TicTacToe = {
 	board: Cell[];
 	currentPlayer: Player;
 }
 
 export function makeMove(state: TicTacToe, index: number): TicTacToe | null {
-	if (state.board[index] !== null) return null; // use structure clone
-
+	if (state.board[index] !== null) return null; 
 	const newState: TicTacToe = structuredClone(state)
 	newState.board[index] = state.currentPlayer;
 	const newPlayer: Player = state.currentPlayer === "X" ? "O" : "X";
 	newState.currentPlayer = newPlayer;
-
 	return newState;
 }
 
-export funciton reset() {
-	this.board = Array(9).fill(null);
-	this.currentPlayer = 'X';
+export function reset(state: TicTacToe): TicTacToe {
+	const newState: TicTacToe = structuredClone(state)
+	newState.board = Array(9).fill(null);
+	newState.currentPlayer = 'X';
+	return newState
 }
